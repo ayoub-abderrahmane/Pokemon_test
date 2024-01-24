09110e503a7f1a5e43b2efa208a4fc1412d884c0
abedel <abedelmouamine.ben-ahmed@laplateforme.io>
2024-01-24 16:17:22 +0100

    Intergration du fichier Pokedex.json

--- Pokedex.py
+++ Pokedex.py
@@ -8,10 +8,29 @@
 import json
 import urllib.request as ul
 
-#Chargement du json
-JSON=ul.urlopen("https://pokebuildapi.fr/api/v1/pokemon")
+#Chargement du json:
+#JSON=ul.urlopen("https://pokebuildapi.fr/api/v1/pokemon")
 
-#Conversion du json en dictionnaire
-data=json.loads(JSON.read())
+#Conversion du json en dictionnaire:
+#data=json.loads(JSON.read())
+#with open('pokedex.json', 'w', encoding='utf-8') as f:
+#    json.dump(data, f, indent=2)
+#print(data[0]['name'])
 
-print (data[0])
+
+
+
+class Pokedex():
+    
+    def __init__(self,):
+        self.data = []
+        with open('pokedex.json', 'r', encoding='utf-8') as f:
+            self.data = json.load(f)
+        
+
+    def affichage(self):
+        print(self.data)
+
+
+pokedex=Pokedex()
+
