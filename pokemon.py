260f85a2d5f503b8aa2002e5dc86274b2f4a9f1c
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-01-24 17:01:53 +0100

    création de la class pokemon & C:/Users/HP/AppData/Local/Programs/Python/Python312/python.exe c:/Users/HP/Documents/Pokemon_test/pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,1 +1,13 @@
-print("Hello")
+import json
+class Test:
+    def __init__(self):
+            self.data = []
+            with open('pokedex.json', 'r', encoding='utf-8') as f:
+                self.data = json.load(f)
+        
+
+    def affichage(self):
+        print(self.data[1] ,{"name"})
+
+test = Test()
+test.affichage()
