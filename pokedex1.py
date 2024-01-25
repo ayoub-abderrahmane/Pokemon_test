6285429ae13986c4be21c0c7f432025955f32436
abedel <abedelmouamine.ben-ahmed@laplateforme.io>
2024-01-25 10:51:17 +0100

    creation d'un fichier temp:pokedex1.py

--- pokedex1.py
+++ pokedex1.py
@@ -8,8 +8,7 @@
 import json
 # import urllib.request as ul
 import random
-import os
-
+import os 
 #Chargement du json:
 #JSON=ul.urlopen("https://pokebuildapi.fr/api/v1/pokemon")
 
@@ -28,16 +27,11 @@ class Pokedex():
         
         with open('pokedex.json', 'r', encoding='utf-8') as f:
             self.data = json.load(f)
-        
-        self.pokedex_joueur = []
-        with open('pokedex_joueur.json', 'a', encoding='utf-8') as f:
-            self.pokedex_joueur = json.load(f)
-        
-    
+
     def pokedex_joueur_existe(self):
         '''Méthode qui vérifie par un bool si pokedex existe déja : 
             -Elle retourne True=existe ,false=existe pas '''
-        
+
         if os.path.exists("pokedex_joueur.json"):
             return True
         else : 
@@ -47,22 +41,20 @@ class Pokedex():
     def pokedex_vide(self):
         '''Méthode permettant de crée un Json vide qui sera le pokedex de départ du joueur.'''
         
-        # self.pokedex_joueur=pokedex_joueur
        
-        if self.pokedex_joueur_existe == False:
 
-            
+        if self.pokedex_joueur_existe()== False:
+       
             #création du pokedex du joueur 
-           
-            # with open('pokedex_joueur.json', 'r', encoding='utf-8') as f:
-            #     json.dump(self.pokedex_joueur, f, indent=2 )
-           
+            self.pokedex_joueur=[]
+
             #Indice[0] = Null       
             self.pokedex_joueur.append("Null")
-            print(self.pokedex_joueur)
-
-           
+            
+            # with open('pokedex_joueur.json', 'a', encoding='utf-8') as f:
+            #     json.dump(self.pokedex_joueur, f, indent=2 )
         
+            
     
     def pokemon_aleatoire (self) :
         '''Choisi un pokemon aléatoirement dans l'api avec toutes ces infos '''
@@ -74,26 +66,27 @@ class Pokedex():
         nb_aleatoire = random.randint(1, total_pokemon)
 
         #Pokemon aléatoire dans le pokedex avec toutes ces infos
-        self.pokemon_aleatoire_ = self.data[nb_aleatoire]
+        self.pokemon_aleatoire = self.data[nb_aleatoire]
        
     
     def ajout_pokemon_rencontrer(self):
-        '''Méthode permettant d'ajouter un pokemon dans pokedex_joueur.'''
-
+        ''' Méthode permettant d'ajouter un pokemon dans pokedex_joueur.'''
+        
         #Ajout du pokemon dans le pokedex        
         with open('pokedex_joueur.json', 'a', encoding='utf-8') as f:
-            self.pokedex_joueur.append(self.pokemon_aleatoire_)
+            self.pokedex_joueur.append(self.pokemon_aleatoire)
             json.dump(self.pokedex_joueur, f, indent=2)
         
     
         # print(self.pokedex_joueur)
 
+    
+    def affichage(self):
+        print(self.data)
 
 
 pokedex=Pokedex()
 
 pokedex.pokedex_vide()
-# pokedex.pokemon_aleatoire() 
-# pokedex.ajout_pokemon_rencontrer()
-
-
+pokedex.pokemon_aleatoire() 
+pokedex.ajout_pokemon_rencontrer()
