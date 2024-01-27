99065e607ecb3ef5d8c43ab1124c74882c41853a
abedel <abedelmouamine.ben-ahmed@laplateforme.io>
2024-01-27 17:14:56 +0100

    Correction des problemes suivants: mise en place d'un filtre pour avoir que les pokemons des 4 types de l'ennoncé | La methode pokemon2 verifie le nombre de pokemon | ajout_pokemon_rencontrer peut etre utiliser sans faire l'appel de toutes les autres méthodes de la class Pokedex

--- pokemon.py
+++ pokemon.py
@@ -1,13 +1,61 @@
 import json
-class Test:
+from Pokedex import Pokedex
+
+class Pokemon(Pokedex):
+    
     def __init__(self):
-            self.data = []
-            with open('pokedex.json', 'r', encoding='utf-8') as f:
-                self.data = json.load(f)
+        super().__init__()        
+        super().ajout_pokemon_rencontrer() 
+        
+        self.pokedex_joueur = []
+        with open('pokedex_joueur.json', 'r', encoding='utf-8') as f:
+            self.pokedex_joueur = json.load(f)
         
 
-    def affichage(self):
-        print(self.data[1] ,{"name"})
+    def pokemon1(self):
+        '''Méthode qui donne les infos principal du pokemon_du_joueur'''
+        
+        self.nom_pokemon1 = self.pokedex_joueur[1]['name']
+        self.image_pokemon1 = self.pokedex_joueur[1]['image']
+        self.pv_pokemon1 = self.pokedex_joueur[1]['stats']['HP']
+        self.attaque_pokemon1 = self.pokedex_joueur[1]['stats']['attack']
+        self.defense_pokemon1 = self.pokedex_joueur[1]['stats']['defense']
+    
+    def pokemon2(self):
+        '''Méthode qui donne les infos principal de l'adversaire:
+            Si nombre total de pokemon dans le pokedex est égale à 1:
+                -On ajoute un pokemon dans le pokedex
+                    
+            Sinon : 
+                -Le dernier pokemon du pokedex devient l'adversaire 
+        '''
+        
+        while True  :    
+           
+            #Nombre total de pokemon dans pokedex
+            Total_pokedex = len(self.pokedex_joueur) - 1
+            
 
-test = Test()
-test.affichage()
+            if Total_pokedex == 1:
+                #on ajoute un pokemon dans le pokedex
+                super().ajout_pokemon_rencontrer()
+            
+            else:
+               
+                self.nom_pokemon1 = self.pokedex_joueur[Total_pokedex]['name']
+                self.image_pokemon1 = self.pokedex_joueur[Total_pokedex]['image']
+                self.pv_pokemon1 = self.pokedex_joueur[Total_pokedex]['stats']['HP']
+                self.attaque_pokemon1 = self.pokedex_joueur[Total_pokedex]['stats']['attack']
+                self.defense_pokemon1 = self.pokedex_joueur[Total_pokedex]['stats']['defense']
+                
+                break
+    
+        
+pokemon=Pokemon()
+
+
+
+    
+
+
+
